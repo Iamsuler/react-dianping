cf1b5b34b94f48564b061461d59952638b59899b
何伟芳 <heweifang0612@163.com>
2020-12-06 23:24:22 +0800

    createReducer优化reducer创建

--- src/redux/modules/entities/products.js
+++ src/redux/modules/entities/products.js
@@ -1,14 +1,22 @@
+import createReducer from '@/utils/createReducer'
+
 export const schema = {
   name: 'products',
   id: 'id'
 }
 
-const reducer = (state = {}, action) => {
-  if (action.response && action.response.products) {
-    return { ...state, ...action.response.products }
-  }
-
-  return state
-}
+const reducer = createReducer(schema.name)
 
 export default reducer
+
+// selector
+export const getProductDetail = (state, id) => {
+  const product = state.entities.products[id]
+
+  // 区分商品列表中的商品和详情商品
+  return product && product.detail ? product : null
+}
+
+export const getProductById = (state, id) => {
+  return state.entities.products[id]
+}
