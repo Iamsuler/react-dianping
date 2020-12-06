719c79cef4b26b3dba209fb9e2c108d6189c7ab2
何伟芳 <heweifang0612@163.com>
2020-12-06 17:43:07 +0800

    完善home的redux定义

--- src/redux/modules/home.js
+++ src/redux/modules/home.js
@@ -24,7 +24,7 @@ export const params = {
 const initState = {
   likes: {
     isFetching: false,
-    pageCount: 0,
+    pageCount: 1,
     ids: []
   },
   discounts: {
@@ -58,19 +58,22 @@ const fetchDiscounts = (endpoint) => ({
 })
 
 export const actions = {
-  fetchLikes: (dispatch, getState) => {
-    const { pageCount } = getState().home.likes
-    const rowIndex = pageCount * params.PAGE_SIZE_LIKES
-    const endpoint = getProdustcList(params.PATH_LIKES, rowIndex, params.PAGE_SIZE_LIKES)
-    dispatch(fetchLikes(endpoint))
+  fetchLikes: () => {
+    return (dispatch, getState) => {
+      const { pageCount } = getState().home.likes
+      const endpoint = getProdustcList(params.PATH_LIKES, pageCount, params.PAGE_SIZE_LIKES)
+      dispatch(fetchLikes(endpoint))
+    }
   },
-  fetchDiscounts: dispatch => {
-    const endpoint = getProdustcList(params.PATH_DISCOUNTS, 0, params.PAGE_SIZE_DISCOUNTS)
-    dispatch(fetchDiscounts(endpoint))
+  fetchDiscounts: () => {
+    return dispatch => {
+      const endpoint = getProdustcList(params.PATH_DISCOUNTS, 0, params.PAGE_SIZE_DISCOUNTS)
+      dispatch(fetchDiscounts(endpoint))
+    }
   }
 }
 
-const likesReducer = (state = initState.likes, action) => {
+const likes = (state = initState.likes, action) => {
   switch (action.type) {
     case types.FETCH_LIKES_PRODUCTS:
       return { ...state, isFetching: true }
@@ -92,18 +95,18 @@ const likesReducer = (state = initState.likes, action) => {
   }
 }
 
-const discountsReducer = (state = initState.discounts, action) => {
+const discounts = (state = initState.discounts, action) => {
   switch (action.type) {
-    case types.FETCH_LIKES_PRODUCTS:
+    case types.FETCH_DISCOUNTS_PRODUCTS:
       return { ...state, isFetching: true }
-    case types.FETCH_LIKES_PRODUCTS_SUCCESS:
+    case types.FETCH_DISCOUNTS_PRODUCTS_SUCCESS:
       const { ids } = state
       return {
         ...state,
         isFetching: false,
         ids: ids.concat(action.response.ids)
       }
-    case types.FETCH_LIKES_PRODUCTS_FAILED:
+    case types.FETCH_DISCOUNTS_PRODUCTS_FAILED:
       return {
         ...state,
         isFetching: false
@@ -114,8 +117,25 @@ const discountsReducer = (state = initState.discounts, action) => {
 }
 
 const homeReducer = combineReducers({
-  likesReducer,
-  discountsReducer
+  likes,
+  discounts
 })
 
 export default homeReducer
+
+// selector
+export const getDiscounts = state => {
+  const { ids } = state.home.discounts
+  const { products } = state.entities
+
+  return ids.map(id => products[id])
+}
+
+export const getLikes = state => {
+  const { ids } = state.home.likes
+  const { products } = state.entities
+
+  return ids.map(id => products[id])
+}
+
+export const getLikesPageCount = state => state.home.likes.pageCount
