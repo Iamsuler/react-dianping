0b272aa2d39f4bac2e60c7dff4a11964ea113876
何伟芳 <heweifang0612@163.com>
2020-12-06 17:43:27 +0800

    home组件调用使用redux

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import Header from './components/Header'
 import Category from './components/Category'
 import Headline from './components/Headline'
@@ -7,8 +9,19 @@ import LikeList from './components/LikeList'
 import Banner from './components/Banner'
 import Activity from './components/Activity'
 
+import { actions as homeActions, getDiscounts, getLikes, getLikesPageCount } from '../../redux/modules/home'
+
 class Home extends Component {
+  fetchLikes = () => {
+    this.props.homeActions.fetchLikes()
+  }
+
+  componentDidMount () {
+    this.props.homeActions.fetchDiscounts()
+  }
+
   render() {
+    const { discounts, likes, pageCount } = this.props
     return (
       <div>
         <Header />
@@ -16,11 +29,30 @@ class Home extends Component {
         <Category />
         <Headline />
         <Activity />
-        <Discount />
-        <LikeList />
+        <Discount data={ discounts } />
+        <LikeList
+          data={ likes }
+          pageCount={ pageCount }
+          fetchLikes={ this.fetchLikes }
+        />
       </div>
     );
   }
 }
 
-export default Home;
+const mapStateToProps = (state) => ({
+  likes: getLikes(state),
+  discounts: getDiscounts(state),
+  pageCount: getLikesPageCount(state)
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  homeActions: bindActionCreators(homeActions, dispatch)
+})
+
+const HomeContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home)
+
+export default HomeContainer;
