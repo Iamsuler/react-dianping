b5d812a6bd02a6fe598eb1047d90bd25eebe8845
何伟芳 <heweifang0612@163.com>
2020-12-06 23:23:17 +0800

    创建公共crateReducer工具函数

---
+++ src/utils/createReducer.js
@@ -0,0 +1,13 @@
+import { createStore } from "redux"
+
+const createReducer = (name) => {
+  return (state = {}, action) => {
+    if (action.response && action.response[name]) {
+      return { ...state, ...action.response[name] }
+    }
+  
+    return state
+  }
+}
+
+export default createReducer
