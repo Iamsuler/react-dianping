a9c92f4e479292bcd854a5b9cd13d31d8bb3ac47
何伟芳 <heweifang0612@163.com>
2020-12-19 13:30:04 +0800

    user页面组件初步完善

--- src/containers/User/index.js
+++ src/containers/User/index.js
@@ -1,13 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import UserHeader from './components/UserHeader'
+import { actions as loginActions } from '../../redux/modules/login'
+import { actions as userActions, getOrders, getStatusText } from '../../redux/modules/user'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
 class User extends Component {
+  handleBack = () => {}
+  handleLogout = () => {
+    this.props.loginActions.logout()
+  }
+
+  componentDidMount() {
+    this.props.userActions.fetchOrders()
+  }
+
   render() {
     return (
       <div>
-        User
+        <UserHeader
+          onBack={ this.handleBack }
+          onLogout={ this.handleLogout }
+        />
       </div>
     );
   }
 }
 
-export default User;
+const mapStateToProps = state => ({
+  orders: getOrders(state),
+  statusText: getStatusText(state)
+})
+const mapDispatchToProps = dispatch => ({
+  loginActions: bindActionCreators(loginActions, dispatch),
+  userActions: bindActionCreators(userActions, dispatch)
+})
+const UserContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(User)
+
+export default UserContainer;
