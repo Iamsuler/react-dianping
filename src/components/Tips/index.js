6fd19a6c2bded7c6380a06612c98004bf5d533c3
何伟芳 <heweifang0612@163.com>
2020-12-19 18:12:58 +0800

    添加tips通用组件

---
+++ src/components/Tips/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './style.css'
+
+class Tips extends Component {
+  render() {
+    const { message, onClose } = this.props
+    return (
+      <div className="tip">
+        <div className="tip__alert">
+          <div className="tip__content">{message}</div>
+          <div className="tip__btns">
+            <button className="tip__btn" onClick={onClose}>确定</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Tips
