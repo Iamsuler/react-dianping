baf4ce7e1eb15cd6ef98a3aecf1b865e3e90fad9
何伟芳 <heweifang0612@163.com>
2020-12-20 14:29:23 +0800

    react官方lazy路由懒加载

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import './style.css';
 import ErrorToast from '../../components/ErrorToast'
 import { getError, actions as appActions } from '../../redux/modules/app'
@@ -6,13 +6,14 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import PrivateRoute from '../PrivateRoute'
-import Home from '../Home'
-import User from '../User'
-import ProductDetail from '../ProductDetail'
-import Search from '../Search'
-import SearchResult from '../SearchResult'
-import Login from '../Login'
-import Purchase from '../Purchase'
+import Loading from '../../components/Loading'
+const Home = lazy(() => import(/* webpackChunkName: "Home" */ '../Home'))
+const User = lazy(() => import(/* webpackChunkName: "User" */ '../User'))
+const ProductDetail = lazy(() => import(/* webpackChunkName: "Home" */ '../ProductDetail'))
+const Search = lazy(() => import(/* webpackChunkName: "Search" */ '../Search'))
+const SearchResult = lazy(() => import(/* webpackChunkName: "Search" */ '../SearchResult'))
+const Login = lazy(() => import(/* webpackChunkName: "Login" */ '../Login'))
+const Purchase = lazy(() => import(/* webpackChunkName: "Purchase" */ '../Purchase'))
 class App extends Component {
   render() {
     const { error, appActions: { clearError } } = this.props
@@ -22,15 +23,17 @@ class App extends Component {
           error ? <ErrorToast message={ error } clearError={ clearError } /> : null
         }
         <BrowserRouter>
-          <Switch>
-            <PrivateRoute path="/user" component={ User } ></PrivateRoute>
-            <PrivateRoute path="/purchase/:id" component={ Purchase } ></PrivateRoute>
-            <Route path="/detail/:id" component={ ProductDetail }></Route>
-            <Route path="/search" component={ Search }></Route>
-            <Route path="/search-result" component={ SearchResult }></Route>
-            <Route path="/login" component={ Login }></Route>
-            <Route path="/" component={ Home }></Route>
-          </Switch>
+          <Suspense fallback={ <Loading /> }>
+            <Switch>
+              <PrivateRoute path="/user" component={ User } ></PrivateRoute>
+              <PrivateRoute path="/purchase/:id" component={ Purchase } ></PrivateRoute>
+              <Route path="/detail/:id" component={ ProductDetail }></Route>
+              <Route path="/search" component={ Search }></Route>
+              <Route path="/search-result" component={ SearchResult }></Route>
+              <Route path="/login" component={ Login }></Route>
+              <Route path="/" component={ Home }></Route>
+            </Switch>
+          </Suspense>
         </BrowserRouter>
       </div>
     );
