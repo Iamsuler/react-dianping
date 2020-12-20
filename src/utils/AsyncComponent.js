aca4a93c418bb979171cd38efca4c44b8d8abae9
何伟芳 <heweifang0612@163.com>
2020-12-20 14:57:39 +0800

    自定义异步加载高阶函数

---
+++ src/utils/AsyncComponent.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+export default function asyncComponent (importComponent) {
+  return class AsyncComponent extends Component {
+    constructor(props) {
+      super(props)
+
+      this.state = {
+        component: null
+      }
+    }
+
+    render() {
+      const Component = this.state.component
+      return Component ? <Component { ...this.props } /> : null
+    }
+
+    componentDidMount() {
+      importComponent().then((mod) => {
+        this.setState({
+          component: mod.default
+        })
+      })
+    }
+  }
+}
